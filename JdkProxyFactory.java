5aa25d28636b50f6bb7453d8c704d6475556c4e2
LYH <3210101621@zju.edu.cn>
2024-04-22 05:20:41 +0800

    update

--- JdkProxyFactory.java
+++ JdkProxyFactory.java
@@ -5,7 +5,7 @@ public class JdkProxyFactory {
         return Proxy.newProxyInstance(
                 target.getClass().getClassLoader(), // 目标类的类加载器
                 target.getClass().getInterfaces(), // 代理需要实现的接口，可指定多个
-                new DebugInvocationHandler(target) // 代理对象对应的自定义 InvocationHandler
+                new MyInvocationHandler(target) // 代理对象对应的自定义 InvocationHandler
         );
     }
 }
--- MyInvocationHandler.java
+++ MyInvocationHandler.java
@@ -2,13 +2,13 @@ import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
-public class DebugInvocationHandler implements InvocationHandler {
+public class MyInvocationHandler implements InvocationHandler {
     /**
      * 代理类中的真实对象
      */
     private final Object target;
 
-    public DebugInvocationHandler(Object target) {
+    public MyInvocationHandler(Object target) {
         this.target = target;
     }
 
